7fb134c47d2bb326ee2ca2d0359ac6166076cb04
Nina Weber <weber_nina@web.de>
2024-05-28 15:36:26 +0200

    define numbers

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const number1 = document.querySelector('[data-js="number1"]');
+const number2 = document.querySelector('[data-js="number2"]');
+const number3 = document.querySelector('[data-js="number3"]');
+const number4 = document.querySelector('[data-js="number4"]');
+const number5 = document.querySelector('[data-js="number5"]');
+const number6 = document.querySelector('[data-js="number6"]');
+const number7 = document.querySelector('[data-js="number7"]');
+const number8 = document.querySelector('[data-js="number8"]');
+const number9 = document.querySelector('[data-js="number9"]');
+
 const numbersOfCalculator =
   document.querySelectorAll(".number-container").length;
 
@@ -7,4 +17,8 @@ for (let i = 0; i < numbersOfCalculator; i++) {
     [i].addEventListener("click", function () {
       alert("I got clicked!");
     });
+}
+
+function calculator(num1, num2, operator) {
+  return operator(num1, num2);
 }
