60c1de2b1c8913eb8271ae48e86a96fc58bbc4c4
Nina Weber <weber_nina@web.de>
2024-05-29 11:41:46 +0200

    add logic to button keypress

--- index.js
+++ index.js
@@ -1,50 +1,82 @@
-const number1 = document.querySelector('[data-js="number1"]');
-const number2 = document.querySelector('[data-js="number2"]');
-const number3 = document.querySelector('[data-js="number3"]');
-const number4 = document.querySelector('[data-js="number4"]');
-const number5 = document.querySelector('[data-js="number5"]');
-const number6 = document.querySelector('[data-js="number6"]');
-const number7 = document.querySelector('[data-js="number7"]');
-const number8 = document.querySelector('[data-js="number8"]');
-const number9 = document.querySelector('[data-js="number9"]');
-const addOperator = document.querySelector('[data-js="add"]');
-const display = document.querySelector('[data-js="display"]');
+// const calculator = document.querySelector(‘.calculator’);
+const keys = document.querySelector(".keys-container");
 
-// number1.addEventListener("click", function () {
-//   return console.log("1");
-// });
+keys.addEventListener("click", function (event) {
+  if (event.target.matches("button")) {
+    const key = event.target;
+    const action = key.dataset.action;
 
-// number2.addEventListener("click", function () {
-//   return console.log("2");
-// });
+    // if no action is found it must be a number key
+    if (!action) {
+      console.log("number key!");
+    }
 
-// function add(num1, num2) {
-//   return num1 + num2;
+    if (
+      action === "add" ||
+      action === "subtract" ||
+      action === "multiply" ||
+      action === "divide"
+    ) {
+      console.log("operator key!");
+    }
+
+    if (action === "clear") {
+      console.log("clear key!");
+    }
+
+    if (action === "solve") {
+      console.log("equal key!");
+    }
+  }
+});
+
+// const number1 = document.querySelector('[data-js="number1"]');
+// const number2 = document.querySelector('[data-js="number2"]');
+// const number3 = document.querySelector('[data-js="number3"]');
+// const number4 = document.querySelector('[data-js="number4"]');
+// const number5 = document.querySelector('[data-js="number5"]');
+// const number6 = document.querySelector('[data-js="number6"]');
+// const number7 = document.querySelector('[data-js="number7"]');
+// const number8 = document.querySelector('[data-js="number8"]');
+// const number9 = document.querySelector('[data-js="number9"]');
+// const addOperator = document.querySelector('[data-js="add"]');
+// const display = document.querySelector('[data-js="display"]');
+
+// // number1.addEventListener("click", function () {
+// //   return console.log("1");
+// // });
+
+// // number2.addEventListener("click", function () {
+// //   return console.log("2");
+// // });
+
+// // function add(num1, num2) {
+// //   return num1 + num2;
+// // }
+
+// const numbersOfCalculator =
+//   document.querySelectorAll(".number-container").length;
+
+// for (let i = 0; i < numbersOfCalculator; i++) {
+//   document
+//     .querySelectorAll(".number-container")
+//     [i].addEventListener("click", function () {
+//       const buttonInnerHTML = this.innerHTML;
+//       // console.log(buttonInnerHTML);
+
+//       switch (buttonInnerHTML) {
+//         case number1:
+//           // const one = document.createElement("p");
+//           // one.append(display);
+//           display.textContent = "1";
+//           break;
+
+//         default:
+//           break;
+//       }
+//     });
 // }
 
-const numbersOfCalculator =
-  document.querySelectorAll(".number-container").length;
-
-for (let i = 0; i < numbersOfCalculator; i++) {
-  document
-    .querySelectorAll(".number-container")
-    [i].addEventListener("click", function () {
-      const buttonInnerHTML = this.innerHTML;
-      // console.log(buttonInnerHTML);
-
-      switch (buttonInnerHTML) {
-        case number1:
-          // const one = document.createElement("p");
-          // one.append(display);
-          display.textContent = "1";
-          break;
-
-        default:
-          break;
-      }
-    });
-}
-
-// function calculator(num1, num2, operator) {
-//   return operator(num1, num2);
-// }
+// // function calculator(num1, num2, operator) {
+// //   return operator(num1, num2);
+// // }
