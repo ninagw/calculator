3a0256020d57fe78117f14107b63a2d55ceb75dc
Nina Weber <weber_nina@web.de>
2024-05-31 12:11:46 +0200

    clean code / index.js

--- index.js
+++ index.js
@@ -75,9 +75,6 @@ keys.addEventListener("click", function (event) {
 
     if (action === "solve") {
       console.log("equal key!");
-      // let firstInputValue = calculator.dataset.firstInputValue;
-      // const operator = calculator.dataset.operator;
-      // const secondInputValue = displayedNumber;
 
       //error handling: only calculate if firstInputValue is given and a number pressed
       if (firstInputValue) {
@@ -128,53 +125,3 @@ function calculate(num1, num2, operator) {
 
   return result;
 }
-
-// const number1 = document.querySelector('[data-js="number1"]');
-// const number2 = document.querySelector('[data-js="number2"]');
-// const number3 = document.querySelector('[data-js="number3"]');
-// const number4 = document.querySelector('[data-js="number4"]');
-// const number5 = document.querySelector('[data-js="number5"]');
-// const number6 = document.querySelector('[data-js="number6"]');
-// const number7 = document.querySelector('[data-js="number7"]');
-// const number8 = document.querySelector('[data-js="number8"]');
-// const number9 = document.querySelector('[data-js="number9"]');
-// const addOperator = document.querySelector('[data-js="add"]');
-
-// // number1.addEventListener("click", function () {
-// //   return console.log("1");
-// // });
-
-// // number2.addEventListener("click", function () {
-// //   return console.log("2");
-// // });
-
-// // function add(num1, num2) {
-// //   return num1 + num2;
-// // }
-
-// const numbersOfCalculator =
-//   document.querySelectorAll(".number-container").length;
-
-// for (let i = 0; i < numbersOfCalculator; i++) {
-//   document
-//     .querySelectorAll(".number-container")
-//     [i].addEventListener("click", function () {
-//       const buttonInnerHTML = this.innerHTML;
-//       // console.log(buttonInnerHTML);
-
-//       switch (buttonInnerHTML) {
-//         case number1:
-//           // const one = document.createElement("p");
-//           // one.append(display);
-//           display.textContent = "1";
-//           break;
-
-//         default:
-//           break;
-//       }
-//     });
-// }
-
-// // function calculator(num1, num2, operator) {
-// //   return operator(num1, num2);
-// // }
